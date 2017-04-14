374329dbf952a95ecc9d9727bdadbafb8fe941c4
callum brennan <callum.brennan@hotmail.co.uk>
2017-04-14 14:51:41 +0100

    added 2 view controllers, and mostly finished setting up the second view controller, then attached it to a new swift file.

---
+++ FilmCollector/FilmViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FilmViewController.swift
+//  FilmCollector
+//
+//  Created by callum brennan on 14/04/2017.
+//  Copyright © 2017 callum brennan Mementcode. All rights reserved.
+//
+
+import UIKit
+
+class FilmViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var gameImageView: UIImageView!
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
