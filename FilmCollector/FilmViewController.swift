184730d32a6fc554ccb206578c414be9caa40bea
callum brennan <callum.brennan@hotmail.co.uk>
2017-04-14 16:37:42 +0100

    we can create film objects and pull them out of core data - and display in first view controller

--- FilmCollector/FilmViewController.swift
+++ FilmCollector/FilmViewController.swift
@@ -8,20 +8,57 @@
 
 import UIKit
 
-class FilmViewController: UIViewController {
+class FilmViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
 
     @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var filmImageView: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    imagePicker.delegate = self
+        
     }
-    @IBOutlet weak var gameImageView: UIImageView!
-
+   
     @IBAction func photosTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
     }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        filmImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+}
+    
+   
     @IBAction func cameraTapped(_ sender: Any) {
     }
+    
     @IBAction func addTapped(_ sender: Any) {
+        // puts a chosen image and title into CoreData, coverts to a PNGR file
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let film = Film(context: context)
+        film.title = titleTextField.text
+        film.image = UIImagePNGRepresentation(filmImageView.image!) as NSData?
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
     }
+    
 }
--- FilmCollector/ViewController.swift
+++ FilmCollector/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var film : [Film] = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            film = try context.fetch(Film.fetchRequest())
+        tableView.reloadData()
+            
+        } catch {
+            
+        }
+
     }
-
-
-}
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return film.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let films = film[indexPath.row]
+        cell.textLabel?.text = films.title
+        cell.imageView?.image = UIImage(data: films.image! as Data )
+        
+        return cell
+        }
+    
+    }
 
